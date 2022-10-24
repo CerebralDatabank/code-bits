22aec0298fe578885d568e3f20a3ed7616c9f9ad
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2022-10-24 07:26:26 -0500

    Fix JS FizzBuzz code to also print 100

--- JavaScript/fizzbuzz-oneliner.js
+++ JavaScript/fizzbuzz-oneliner.js
@@ -1,1 +1,1 @@
-console.log([...Array(100).keys()].slice(1).map(n => !(n % 3) ? (!(n % 5) ? "FizzBuzz" : "Fizz") : (!(n % 3) ? "Buzz" : n)).join("\n"));
+console.log([...Array(100).keys()].map(n => n + 1).map(n => !(n % 3) ? (!(n % 5) ? "FizzBuzz" : "Fizz") : (!(n % 3) ? "Buzz" : n)).join("\n"));
