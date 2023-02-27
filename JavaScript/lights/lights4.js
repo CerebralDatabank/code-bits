cd57dfb802a7d1d11c36f37fcfa36a83f63cf6ca
Gopal Othayoth <Gopal.Othayoth@outlook.com>
2023-02-26 18:41:34 -0600

    Fix lights one-liner to include last person

--- JavaScript/lights/lights4.js
+++ JavaScript/lights/lights4.js
@@ -6,4 +6,4 @@ const ROWS = 18;
 const COLS = 24;
 const TOTAL = 432;
 
-console.log([...Array(TOTAL).keys()].map((_, i, mainArr) => mainArr[i] = i == 0 ? Array(TOTAL).fill(false) : mainArr[i - 1].map((_, j) => ((j + 1) % i == 0) != mainArr[i - 1][j])).map((e, i) => e.map(b => b ? LIGHT_ON : LIGHT_OFF).join("").match(new RegExp(`.{${COLS}}`, "g")).join("\n")).map((e, i) => [i == 0 ? "Initial state" : `\nPerson ${i}`, e]).flat().join("\n"));
+console.log([...Array(TOTAL + 1).keys()].map((_, i, mainArr) => mainArr[i] = i == 0 ? Array(TOTAL + 1).fill(false) : mainArr[i - 1].map((_, j) => ((j + 1) % i == 0) != mainArr[i - 1][j])).map((e, i) => e.map(b => b ? LIGHT_ON : LIGHT_OFF).join("").match(new RegExp(`.{${COLS}}`, "g")).join("\n")).map((e, i) => [i == 0 ? "Initial state" : `\nPerson ${i}`, e]).flat().join("\n"));
